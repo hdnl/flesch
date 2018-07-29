37220f0aee79b263deb0312763153448b35fdab2
Hayden Le <haydlite@users.noreply.github.com>
2018-07-28 22:04:24 -0500

    score determined

---
+++ scripts/readability.js
@@ -0,0 +1,42 @@
+function syllable_count(text) {
+    let words = text.split(/,| /);
+    let numOfSyllables = 0;
+    for(const word of words) {
+        let currWord = word.toLowerCase();
+        if(currWord.length <= 3) {
+            numOfSyllables += 1;
+            continue;
+        }
+        currWord = currWord.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
+        currWord = currWord.replace(/^y/, '');
+        numOfSyllables += currWord.match(/[aeiouy]{1,2}/g).length;
+    }
+
+    return numOfSyllables;
+}
+
+function score(text) {
+    let totalWords = text.split(' ').length;
+    let totalSentences = 1;
+    if(text.split('.').length > 1) {
+        totalSentences = text.split('.').length;
+    }
+    let totalSyllables = syllable_count(text);
+    console.log('totalWords: ' + totalWords);
+    console.log('totalSentences: ' + totalSentences);
+    console.log('totalSyllables: ' + totalSyllables);
+    return 206.835 - 1.015*(totalWords/totalSentences) - 84.6*(totalSyllables/totalWords)
+}
+
+$(function() {
+    let text;
+
+    console.log( "ready!" );
+    $(document).on('keyup', '#str', function() {
+        text = $('#str').val();
+        console.log(text);
+        if(text.length > 0) {
+            $('#currentReadingLevel').html('Current Reading Level: ' + score(text));
+        }
+    });
+});
